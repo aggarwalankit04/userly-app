e304e1d7b03f2a7e01b562989d16833f5b6c4eac
ankaggar3 <GFGexample@gmai>
2022-07-11 11:39:03 +0530

    final commit

--- src/App.vue
+++ src/App.vue
@@ -16,62 +16,12 @@
         v-for="randomuser in randomuserData"
         :key="randomuser.id.value"
       >
-        <div class="card">
-          <picture class="text-center">
-            <source
-              media="screen and (min-width:992px)"
-              :srcset="randomuser.picture.large"
-            />
-            <source
-              media="screen and (min-width:768px)"
-              :srcset="randomuser.picture.medium"
-            />
-            <source
-              media="screen and (min-width:576px)"
-              :srcset="randomuser.picture.thumbnail"
-            />
-            <img
-              :src="randomuser.picture.thumbnail"
-              alt="MEN"
-              class="rounded-circle"
-            />
-          </picture>
-          <div class="card-body">
-            <h6
-              class="card-title"
-              v-bind:style="{ 'background-color': colorChoiced }"
-            >
-              <i class="fa fa-user"></i>
-              {{
-                `${randomuser.name.title} ${randomuser.name.first} ${randomuser.name.last}`
-              }}
-            </h6>
-            <p
-              class="card-subtitle mb-2 text-muted"
-              v-bind:style="{ 'background-color': colorChoiced }"
-            >
-              <i class="fa-solid fa-phone"></i>
-              {{ randomuser.phone }}
-            </p>
-            <p
-              class="card-subtitle mb-2 text-muted"
-              v-bind:style="{ 'background-color': colorChoiced }"
-            >
-              <i class="fa-solid fa-at"></i>
-              {{ randomuser.email }}
-            </p>
-            <p
-              class="card-subtitle mb-2 text-muted"
-              v-bind:style="{ 'background-color': colorChoiced }"
-            >
-              <i class="fa-solid fa-location-dot"></i
-              >{{
-                `${randomuser.location.street.number},${randomuser.location.street.name} ${randomuser.location.city}, ${randomuser.location.state} ${randomuser.location.country} ${randomuser.location.postcode}`
-              }}
-            </p>
-          </div>
-        </div>
+        <user-cards
+          :randomuser="randomuser"
+          :colorChoiced="colorChoiced"
+        ></user-cards>
       </div>
+
       <div class="col-1 col-sm-1 col-md-1 col-lg-1 arrow-right">
         <i
           class="fa-solid fa-arrow-right"
@@ -85,11 +35,13 @@
 <script>
 import axios from "axios";
 import ColorForm from "./components/Colorform.vue";
+import UserCards from "./components/UserCards.vue";
 
 export default {
   name: "App",
   components: {
     ColorForm,
+    UserCards,
   },
   data() {
     return {
---
+++ src/components/UserCards.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card">
+    <picture class="text-center">
+      <source
+        media="screen and (min-width:992px)"
+        :srcset="randomuser.picture.large"
+      />
+      <source
+        media="screen and (min-width:768px)"
+        :srcset="randomuser.picture.medium"
+      />
+      <source
+        media="screen and (min-width:576px)"
+        :srcset="randomuser.picture.thumbnail"
+      />
+      <img
+        :src="randomuser.picture.thumbnail"
+        alt="MEN"
+        class="rounded-circle"
+      />
+    </picture>
+    <div class="card-body">
+      <h6
+        class="card-title"
+        v-bind:style="{ 'background-color': colorChoiced }"
+      >
+        <i class="fa fa-user"></i>
+        {{
+          `${randomuser.name.title} ${randomuser.name.first} ${randomuser.name.last}`
+        }}
+      </h6>
+      <p
+        class="card-subtitle mb-2 text-muted"
+        v-bind:style="{ 'background-color': colorChoiced }"
+      >
+        <i class="fa-solid fa-phone"></i>
+        {{ randomuser.phone }}
+      </p>
+      <p
+        class="card-subtitle mb-2 text-muted"
+        v-bind:style="{ 'background-color': colorChoiced }"
+      >
+        <i class="fa-solid fa-at"></i>
+        {{ randomuser.email }}
+      </p>
+      <p
+        class="card-subtitle mb-2 text-muted"
+        v-bind:style="{ 'background-color': colorChoiced }"
+      >
+        <i class="fa-solid fa-location-dot"></i
+        >{{
+          `${randomuser.location.street.number},${randomuser.location.street.name} ${randomuser.location.city}, ${randomuser.location.state} ${randomuser.location.country} ${randomuser.location.postcode}`
+        }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCards",
+  props: { randomuser: Object, colorChoiced: String },
+};
+</script>
